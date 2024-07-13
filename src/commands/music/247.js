e1a742a4ccf09cfc44e3b95aa92e4e1a701cff0a
AikooNee <159515125+AikooNee@users.noreply.github.com>
2024-07-13 19:53:54 +0530

    Fixed 247

--- src/commands/music/247.js
+++ src/commands/music/247.js
@@ -35,8 +35,17 @@ module.exports = {
  * @param {import("discord.js").CommandInteraction|import("discord.js").Message} arg0
  * @param {Object} settings
  */
-async function toggle247({ client, guild }, settings) {
+async function toggle247({ client, guildId, member, channel }, settings) {
   const is247 = settings.music.stay.enabled;
+
+  // Check if the user is connected to a voice channel
+  if (!member.voice?.channelId) {
+    const embed = new EmbedBuilder()
+      .setColor(client.config.EMBED_COLORS.ERROR)
+      .setDescription("You need to be connected to a voice channel to toggle 24/7 mode.");
+
+    return { embeds: [embed] };
+  }
 
   settings.music.stay.enabled = !is247;
   await settings.save();
@@ -45,6 +54,15 @@ async function toggle247({ client, guild }, settings) {
   const embed = new EmbedBuilder()
     .setColor(client.config.EMBED_COLORS.BOT_EMBED)
     .setDescription(description);
+
+  if (!is247) {
+    let player = client.musicManager.players.resolve(guildId);
+    if (!player?.connected) {
+      player = client.musicManager.players.create(guildId);
+      player.queue.data.channel = channel;
+      player.voice.connect(member.voice.channel.id, { deafened: true });
+    }
+  }
 
   return { embeds: [embed] };
 }
