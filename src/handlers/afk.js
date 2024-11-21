429feda0316074e858b6da17cbc3126dd8f0c324
Aiko <159515125+aikoonee@users.noreply.github.com>
2024-11-21 13:20:53 +0530

    improve ><

--- src/handlers/afk.js
+++ src/handlers/afk.js
@@ -1,18 +1,17 @@
 const { EmbedBuilder } = require("discord.js");
 const { timeformat } = require("@helpers/Utils");
+const { EMBED_COLORS } = require("@root/config");
 
 const afk = async (client, message, settings) => {
   if (!message.guild || message.author.bot) return;
 
-  const userId = message.author.id;
-  const afkEntry = settings.afk.find((a) => a.userId === userId);
-  const botEmbedColor = client.config.EMBED_COLORS.BOT_EMBED;
+  const afkEntry = settings.afk.find((a) => a.userId === message.author.id);
 
   if (afkEntry && afkEntry.status === true) {
     const duration = Math.floor((Date.now() - new Date(afkEntry.afktime)) / 1000);
 
     if (duration >= 2) {
-      let description = `Welcome back, <@${userId}>! You were away for ${timeformat(duration)}`;
+      let description = `Welcome back, <@${message.author.id}>! You were away for ${timeformat(duration)}`;
 
       if (afkEntry.msglink && afkEntry.msglink.length > 0) {
         description += "\n\nWhile you were away, you received the following messages";
@@ -21,13 +20,14 @@ const afk = async (client, message, settings) => {
         });
       }
 
-      settings.afk = settings.afk.filter((a) => a.userId !== userId);
+      settings.afk = settings.afk.filter((a) => a.userId !== message.author.id);
       await settings.save();
 
-      const embed = new EmbedBuilder().setColor(botEmbedColor).setDescription(description);
-
-      await message.safeReply({ embeds: [embed] });
-      return;
+      return message.safeReply({
+        embeds: [
+          new EmbedBuilder().setColor(EMBED_COLORS.BOT_EMBED).setDescription(description),
+        ],
+      });
     }
   }
 
@@ -39,9 +39,11 @@ const afk = async (client, message, settings) => {
       const duration = Math.floor((Date.now() - new Date(afkCheck.afktime)) / 1000);
       const description = `<@${user.id}> has been AFK for ${timeformat(duration)}.\nReason: ${afkCheck.reason}`;
 
-      const embed = new EmbedBuilder().setColor(botEmbedColor).setDescription(description);
-
-      await message.safeReply({ embeds: [embed] });
+      await message.safeReply({
+        embeds: [
+          new EmbedBuilder().setColor(EMBED_COLORS.BOT_EMBED).setDescription(description),
+        ],
+      });
 
       const messageLink = `https://discord.com/channels/${message.guild.id}/${message.channel.id}/${message.id}`;
       afkCheck.msglink.push(messageLink);
